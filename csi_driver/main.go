0056010105634bb0afeb1960a120608e4df96e49
gosub2k <31038611+gosub2k@users.noreply.github.com>
2026-06-06 14:26:23 -0400

    feat: add slog logging to CSI driver
    Executed by Claude Sonnet 4.6 <noreply@anthropic.com>

--- csi_driver/main.go
+++ csi_driver/main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-    "flag"
-    "net"
-    "os"
-    "github.com/container-storage-interface/spec/lib/go/csi"
-    "google.golang.org/grpc"
+	"flag"
+	"log/slog"
+	"net"
+	"os"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc"
 )
 
 var (
-    endpoint = flag.String("endpoint", "unix:///var/lib/kubelet/plugins/local~freeport/csi.sock", "CSI endpoint")
-    nodeID   = flag.String("nodeid", "", "Node ID")
-    name     = flag.String("name", "local/freeport", "Driver name")
-    version  = flag.String("version", "0.1.0", "Driver version")
+	endpoint = flag.String("endpoint", "unix:///var/lib/kubelet/plugins/local~freeport/csi.sock", "CSI endpoint")
+	nodeID   = flag.String("nodeid", "", "Node ID")
+	name     = flag.String("name", "local/freeport", "Driver name")
+	version  = flag.String("version", "0.1.0", "Driver version")
 )
 
+var log = slog.Default()
+
 func main() {
-    flag.Parse()
-    sock := (*endpoint)[7:] // strip "unix://"
-    os.Remove(sock)
+	flag.Parse()
+	sock := (*endpoint)[7:] // strip "unix://"
+	os.Remove(sock)
 
-    lis, err := net.Listen("unix", sock)
-    if err != nil {
-        panic(err)
-    }
+	lis, err := net.Listen("unix", sock)
+	if err != nil {
+		log.Error("failed to listen", "socket", sock, "err", err)
+		os.Exit(1)
+	}
 
-    server := grpc.NewServer()
-    csi.RegisterIdentityServer(server, NewIdentityServer(*name, *version))
-    csi.RegisterNodeServer(server, NewNodeServer(*nodeID))
+	log.Info("starting", "driver", *name, "version", *version, "node", *nodeID, "socket", sock)
 
-    server.Serve(lis)
+	server := grpc.NewServer()
+	csi.RegisterIdentityServer(server, NewIdentityServer(*name, *version))
+	csi.RegisterNodeServer(server, NewNodeServer(*nodeID))
+
+	server.Serve(lis)
 }
